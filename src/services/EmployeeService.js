45c624cd5977f8f8b04402a30ffb95ca28192335
gokulkrishna12 <gokuldinesh32@gmail.com>
2026-06-22 17:30:50 +0530

    Updated backend API URL

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const REST_API_BASE_URL =
-  "http://localhost:8080/api/employees";
+  "employee-management-system-backend-production-f0ac.up.railway.app";
 
 export const listEmployees = () =>
   axios.get(REST_API_BASE_URL);
